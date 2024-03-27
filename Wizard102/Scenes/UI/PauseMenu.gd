2a0910a6a164d74176c5f4b61d9c336affb85587
PatricNurczyk <115892428+PatricNurczyk@users.noreply.github.com>
2024-03-27 12:42:32 -0400

    Fixed pause menu glitch where you could click it when closed

--- Wizard102/Scenes/UI/PauseMenu.gd
+++ Wizard102/Scenes/UI/PauseMenu.gd
@@ -3,11 +3,13 @@ extends Control
 func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	get_parent().visible = false
 	
 func pause():
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
 	$PanelContainer/VBoxContainer/Resume.grab_focus()
+	get_parent().visible = true
 	
 func testEsc():
 	if Input.is_action_just_pressed("esc") and !get_tree().paused:
