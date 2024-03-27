774a6921b7964f5075fb17edf64a0ba2b838aaae
chris-canty <chriscanty3@gmail.com>
2024-03-26 22:43:03 -0400

    Fixed restart glitch

--- Wizard102/Scenes/UI/PauseMenu.gd
+++ Wizard102/Scenes/UI/PauseMenu.gd
@@ -21,6 +21,7 @@ func _on_resume_pressed():
 func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+	$AnimationPlayer.seek(0, true) 
 	
 func _on_quit_pressed():
 	get_tree().paused = false
