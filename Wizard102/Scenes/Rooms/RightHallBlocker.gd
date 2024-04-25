5d6d134ef20013155104160f53d4e616a63647ce
James Hugglestone <jawh3@hugglestone.com>
2024-04-25 19:06:16 -0400

    Left and right halls are good

---
+++ Wizard102/Scenes/Rooms/RightHallBlocker.gd
@@ -0,0 +1,14 @@
+extends CollisionPolygon2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if RoomInfo.curr_x == 4:
+		disabled = false
+	else:
+		disabled = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
