736b8dc2b50a9ae2e1981232219cc7481255b0e5
PatricNurczyk <115892428+PatricNurczyk@users.noreply.github.com>
2024-04-27 14:27:30 -0400

    Added MiniMap

--- Wizard102/Scripts/GameContainer.gd
+++ Wizard102/Scripts/GameContainer.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @onready var screen = $"Black Screen/Fade"
+@onready var ui_rooms = $"room_ui/VBoxContainer/rooms"
+@onready var ui_boss = $"room_ui/VBoxContainer/Boss"
 var current_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Loaded Main Game Container")
 	await RoomInfo.generate_floor()
+	for node in ui_rooms.get_children():
+		node.modulate = Color("000000")
 	load_room_first()
 
 
@@ -14,6 +18,17 @@ func _ready():
 func _process(delta):
 	pass
 	
+func set_ui():
+	if RoomInfo.curr_y == 5:
+		ui_boss.modulate = Color("ffffff")
+	else:
+		ui_boss.modulate = Color("000000")
+	for node in ui_rooms.get_children():
+		if node.get_name() == (str(RoomInfo.curr_x) + "-" + str(RoomInfo.curr_y)):
+			node.modulate = Color("ffffff")
+		else:
+			node.modulate = Color("000000")
+			
 func load_room_first():
 	RoomInfo.curr_x = 2
 	RoomInfo.curr_y = 0
@@ -22,6 +37,7 @@ func load_room_first():
 	for enemy in get_node("basic_room").get_tree().get_nodes_in_group("Enemy"):
 		enemy.queue_free()
 	get_node("basic_room").get_node("Player").position = PlayerData.player_start_position
+	set_ui()
 	screen.play("fade_in")
 	
 func load_boss_room():
@@ -31,6 +47,7 @@ func load_boss_room():
 	var player_pos = PlayerData.player_start_position
 	await add_child(current_node)
 	get_node("basic_room").get_node("Player").position = player_pos
+	set_ui()
 	screen.play("fade_in")
 		
 func load_room(x: int, y: int):
@@ -53,6 +70,7 @@ func load_room(x: int, y: int):
 	var player_pos = PlayerData.player_start_position
 	await add_child(current_node)
 	get_node("basic_room").get_node("Player").position = player_pos
+	set_ui()
 	screen.play("fade_in")
 	print(x,y)
 
